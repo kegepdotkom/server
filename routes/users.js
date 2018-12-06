09acddc1235585a28edba0b8cf90f09dd3af585f
Atras Tudhi <atras.r@gmail.com>
2018-12-06 19:59:04 +0700

    login register edit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const userController = require('../controller/userController')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', userController.register)
+router.post('/login', userController.login)
+router.put('/:id', userController.edit)
 
 module.exports = router;
